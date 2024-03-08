6b1a069a9ed5bcaa00dfaf3375180bfa89f59dad
ratul16 <haratul@gmail.com>
2024-03-08 01:43:24 +0000

    Updated component to handle add & edit

--- src/components/AddEditForm.vue
+++ src/components/AddEditForm.vue
@@ -1,141 +1,365 @@
 <template>
-  <div class="add-edit-form">
-    <!-- <h4>Add Edit Form</h4> -->
-    <section>
-      <h6>Bill From</h6>
-      <div class="row g-3">
-        <div class="col-md-12">
-          <label for="address" class="form-label">Street Address</label>
-          <input type="text" class="form-control" id="address" />
-        </div>
-        <div class="col-md-4">
-          <label for="city" class="form-label">City</label>
-          <input type="text" class="form-control" id="city" />
-        </div>
-        <div class="col-md-4">
-          <label for="postcode" class="form-label">Post Code</label>
-          <input type="text" class="form-control" id="postcode" />
-        </div>
-        <div class="col-md-4">
-          <label for="country" class="form-label">Country</label>
-          <input type="text" class="form-control" id="country" />
-        </div>
-      </div>
-    </section>
+  <div class="form-side-bar" :class="isVisible ? 'active' : ''">
+    <div class="form-body">
+      <h4 v-if="invoice" class="mb-4">
+        {{ invoice.id ? "Edit" : "New Invoice" }}
+        <b>#</b>{{ invoice.id }}
+      </h4>
+      <div class="add-edit-form">
+        <!-- <h4>Add Edit Form</h4> -->
+        <!-- {{ formData }} -->
+        <section>
+          <h6>Bill From</h6>
+          <div class="row g-3">
+            <div class="col-md-12">
+              <label for="address" class="form-label">Street Address</label>
+              <input
+                type="text"
+                class="form-control"
+                id="address"
+                v-model="formData.senderAddress.street"
+              />
+            </div>
+            <div class="col-md-4">
+              <label for="city" class="form-label">City</label>
+              <input
+                type="text"
+                class="form-control"
+                id="city"
+                v-model="formData.senderAddress.city"
+              />
+            </div>
+            <div class="col-md-4">
+              <label for="postcode" class="form-label">Post Code</label>
+              <input
+                type="text"
+                class="form-control"
+                id="postcode"
+                v-model="formData.senderAddress.postCode"
+              />
+            </div>
+            <div class="col-md-4">
+              <label for="country" class="form-label">Country</label>
+              <input
+                type="text"
+                class="form-control"
+                id="country"
+                v-model="formData.senderAddress.country"
+              />
+            </div>
+          </div>
+        </section>
 
-    <section>
-      <h6 class="heading-s">Bill To</h6>
-      <div class="row g-3">
-        <div class="col-md-12">
-          <label for="name" class="form-label">Client's Name</label>
-          <input type="text" class="form-control" id="name" />
-        </div>
-        <div class="col-md-12">
-          <label for="email" class="form-label">Client's Email</label>
-          <input type="email" class="form-control" id="email" />
-        </div>
-        <div class="col-md-12">
-          <label for="address" class="form-label">Street Address</label>
-          <input type="text" class="form-control" id="address" />
-        </div>
-        <div class="col-md-4">
-          <label for="city" class="form-label">City</label>
-          <input type="text" class="form-control" id="city" />
-        </div>
-        <div class="col-md-4">
-          <label for="postcode" class="form-label">Post Code</label>
-          <input type="text" class="form-control" id="postcode" />
-        </div>
-        <div class="col-md-4">
-          <label for="country" class="form-label">Country</label>
-          <input type="text" class="form-control" id="country" />
-        </div>
-        <div class="col-md-6">
-          <label for="date" class="form-label">Invoice Date</label>
-          <DatePickerComponent />
-        </div>
-        <div class="col-md-6">
-          <label for="terms" class="form-label">Payment Terms</label>
-          <SelectComponent />
-        </div>
-      </div>
-    </section>
+        <section>
+          <h6 class="heading-s">Bill To</h6>
+          <div class="row g-3">
+            <div class="col-md-12">
+              <label for="name" class="form-label">Client's Name</label>
+              <input type="text" class="form-control" id="name" v-model="formData.clientName" />
+            </div>
+            <div class="col-md-12">
+              <label for="email" class="form-label">Client's Email</label>
+              <input type="email" class="form-control" id="email" v-model="formData.clientEmail" />
+            </div>
+            <div class="col-md-12">
+              <label for="address" class="form-label">Street Address</label>
+              <input
+                type="text"
+                class="form-control"
+                id="address"
+                v-model="formData.clientAddress.street"
+              />
+            </div>
+            <div class="col-md-4">
+              <label for="city" class="form-label">City</label>
+              <input
+                type="text"
+                class="form-control"
+                id="city"
+                v-model="formData.clientAddress.city"
+              />
+            </div>
+            <div class="col-md-4">
+              <label for="postcode" class="form-label">Post Code</label>
+              <input
+                type="text"
+                class="form-control"
+                id="postcode"
+                v-model="formData.clientAddress.postCode"
+              />
+            </div>
+            <div class="col-md-4">
+              <label for="country" class="form-label"> Country </label>
+              <input
+                type="text"
+                class="form-control"
+                id="country"
+                v-model="formData.clientAddress.country"
+              />
+            </div>
+            <div class="col-md-6">
+              <label for="date" class="form-label">Invoice Date</label>
+              <DatePickerComponent :paymentDue="formData.paymentDue" @dateUpdated="dateUpdated" />
+            </div>
+            <div class="col-md-6">
+              <label for="terms" class="form-label">Payment Terms</label>
+              <select v-model="formData.paymentTerms" class="form-select" id="terms">
+                <option value="1">Net 1 Day</option>
+                <option value="7">Net 7 Days</option>
+                <option value="14">Net 14 Days</option>
+                <option value="30">Net 30 Days</option>
+              </select>
+            </div>
+            <div class="col-md-12">
+              <label for="country" class="form-label"> Project Descriptions </label>
+              <input type="text" class="form-control" id="country" v-model="formData.description" />
+            </div>
+          </div>
+        </section>
 
-    <section>
-      <h6>Item List</h6>
-      <div class="row g-3 justify-content-between align-items-center">
-        <div class="col-md-4">
-          <label for="address" class="form-label">Item Name</label>
-          <input type="text" class="form-control" id="address" />
-        </div>
-        <div class="col-md-2">
-          <label for="city" class="form-label">Qty</label>
-          <input type="text" class="form-control" id="city" />
-        </div>
-        <div class="col-md-2">
-          <label for="postcode" class="form-label">Price</label>
-          <input type="text" class="form-control" id="postcode" />
-        </div>
-        <div class="col-md-3">
-          <label for="country" class="form-label">Total</label>
-          <input type="text" class="form-control" id="country" />
-        </div>
-        <div class="col-md-1">
-          <i class="fa-solid fa-trash"></i>
-        </div>
-        <div class="col-md-12">
-          <button class="btn draft btn-block">
-            <i class="fa-solid fa-plus"></i>
-            Add New Item
-          </button>
+        <section>
+          <h6>Item List</h6>
+          <div class="row g-3">
+            <div class="col-md-12">
+              <div class="row justify-content-between align-items-center">
+                <div class="col-md-4">
+                  <label for="item-name" class="form-label">Item Name</label>
+                </div>
+                <div class="col-md-2">
+                  <label for="qty" class="form-label">Qty</label>
+                </div>
+                <div class="col-md-3">
+                  <label for="price" class="form-label">Price</label>
+                </div>
+                <div class="col-md-2">
+                  <label for="total" class="form-label">Total</label>
+                </div>
+                <div class="col-md-1"></div>
+              </div>
+              <div
+                class="row justify-content-between align-items-center mb-2"
+                v-for="(i, index) in formData.items"
+                :key="index"
+              >
+                <div class="col-md-4">
+                  <input
+                    type="text"
+                    class="form-control"
+                    :id="`item-name-${index}`"
+                    v-model="i.name"
+                  />
+                </div>
+                <div class="col-md-2">
+                  <input
+                    type="number"
+                    class="form-control"
+                    :id="`qty-${index}`"
+                    v-model="i.quantity"
+                  />
+                </div>
+                <div class="col-md-3">
+                  <input
+                    type="number"
+                    class="form-control"
+                    :id="`price-${index}`"
+                    v-model="i.price"
+                  />
+                </div>
+                <div class="col-md-2">£ {{ i.quantity * i.price }}</div>
+                <div class="col-md-1" :id="index">
+                  <i class="fa-solid fa-trash"></i>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <button class="btn draft btn-block" @click="addNewItem">
+                <i class="fa-solid fa-plus"></i>
+                Add New Item
+              </button>
+            </div>
+          </div>
+        </section>
+        <div class="form-footer">
+          <button v-if="invoice && !invoice.id" class="btn danger" @click="close">Discard</button>
+          <button class="btn draft" @click="close">Cancel</button>
+          <button class="btn primary" @click="addEditInvoice">Save Changes</button>
         </div>
       </div>
-    </section>
-    <div class="form-footer">
-      <button class="btn left">Discard</button>
-      <button class="btn draft">Cancel</button>
-      <button class="btn primary">Save Changes</button>
     </div>
   </div>
 </template>
 
 <script>
 import DatePickerComponent from "./DatePickerComponent.vue";
-import SelectComponent from "./SelectComponent.vue";
 
 export default {
   name: "AddEditForm",
   components: {
     DatePickerComponent,
-    SelectComponent,
+  },
+  props: {
+    isVisible: {
+      type: Boolean,
+      default: false,
+    },
+    invoice: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      formData: {
+        senderAddress: {
+          street: "",
+          city: "",
+          postCode: "",
+          country: "",
+        },
+        clientAddress: {
+          street: "",
+          city: "",
+          postCode: "",
+          country: "",
+        },
+        items: [{ name: "", quantity: 0, price: 0, total: 0 }],
+      },
+    };
+  },
+  mounted() {
+    if (this.isVisible && this.invoice.id) {
+      this.formData = JSON.parse(JSON.stringify(this.invoice));
+      // console.log(this.invoice);
+    }
+  },
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+    dateUpdated(val) {
+      this.formData.paymentDue = val;
+    },
+    addNewItem() {
+      this.formData.items.push({ name: "", quantity: 0, price: 0, total: 0 });
+    },
+    addEditInvoice() {
+      let data = JSON.parse(JSON.stringify(this.formData));
+      data.items = data.items.map((x) => {
+        return {
+          name: x.name,
+          quantity: x.quantity,
+          price: x.price,
+          total: x.price * x.quantity,
+        };
+      });
+      data.paymentTerms = Number(data.paymentTerms);
+      this.$emit("updateInvoice", data);
+      this.close();
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.add-edit-form {
+.form-side-bar {
+  ::-webkit-scrollbar {
+    width: 0px;
+  }
+
   position: relative;
+  z-index: 5;
+  transition: all 0.3s ease-in-out;
 
-  h6 {
-    color: $purple;
+  .form-body {
+    overflow-y: scroll;
+    position: fixed;
+    left: 75px;
+    top: 0;
+    width: 620px;
+    height: 100%;
+    background-color: $bg-color;
+    box-shadow: -80px 0px $bg-color;
+    border-radius: 0 20px 20px 0;
+    padding: 30px;
+    translate: -700px;
+    transition: all 0.3s ease-in-out;
+    // .form-footer {
+    //   position: absolute;
+    //   bottom: 0;
+    //   width: ;
+    //   max-height: 100px;
+    //   background: red;
+    //   padding: 20px;
+
+    //   &.add,
+    //   .edit {
+    //     widows: 100%;
+    //     display: flex;
+    //     align-items: center;
+    //     justify-content: space-between;
+    //     .draft {
+    //       margin-right: 10px;
+    //     }
+    //   }
+    // }
   }
 
-  section {
-    margin-bottom: 26px;
-  }
-
-  .form-footer {
+  &::before {
+    content: "";
+    position: relative;
+    left: 0;
+    top: 0;
     width: 100%;
-    display: flex;
-    justify-content: space-between;
-    gap: 10px;
+    height: 100%;
+    background-color: $black;
+    opacity: 50%;
+    z-index: -1;
+  }
 
-    .left {
-      margin-right: auto;
+  &.active {
+    .form-body {
+      translate: 0;
     }
 
-    .draft {
-      margin-left: auto;
+    &::before {
+      content: "";
+      position: fixed;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      background-color: $black;
+      opacity: 50%;
+      z-index: -1;
+    }
+  }
+
+  .add-edit-form {
+    h6 {
+      color: $purple;
+    }
+
+    section {
+      margin-bottom: 26px;
+    }
+
+    .form-footer {
+      position: sticky;
+      right: 0;
+      bottom: -10px;
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      gap: 10px;
+      background-color: $bg-color;
+      // padding: 10px 0;
+      box-shadow:
+        0px 20px $bg-color,
+        0px -20px $bg-color;
+
+      .draft {
+        margin-left: auto;
+      }
     }
   }
 }
